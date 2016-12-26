60ece169e315976776ad974447978700ebf23cac
kuro-neko <shirocyara@yahoo.co.jp>
2016-12-26 11:10:42 +0900

    fix warning

--- src/mergedoc/ui/ComponentFactory.java
+++ src/mergedoc/ui/ComponentFactory.java
@@ -17,113 +17,133 @@ import javax.swing.JScrollPane;
 
 /**
  * コンポーネントファクトリです。
+ * 
  * @author Shinji Kashihara
  */
 public class ComponentFactory {
 
-    /** このアプリケーションがサポートするコンポーネント最大サイズ（不変） */
-    private static final Dimension maxDimension = new Dimension(3200, 2400) {
-        public void setSize(Dimension d) {}
-        public void setSize(double width, double height) {}
-        public void setSize(int width, int height) {}
-        public void setSize(Dimension2D d) {}
-    };
+	/** このアプリケーションがサポートするコンポーネント最大サイズ（不変） */
+	private static final Dimension maxDimension = new Dimension(3200, 2400) {
+		public void setSize(Dimension d) {
+		}
 
-    /**
-     * コンストラクタです。生成不可。
-     */
-    private ComponentFactory() {
-    }
+		public void setSize(double width, double height) {
+		}
 
-    /**
-     * このアプリケーションがサポートするコンポーネント最大サイズを作成します。
-     * この Dimension は通常の Dimension と異なりサイズを変更するメソッドを
-     * 呼び出してもサイズが変わらない不変クラスです。
-     * @return コンポーネントの最大サイズ
-     */
-    public static Dimension createMaxDimension() {
-        return maxDimension;
-    }
-    
-    /**
-     * サイズ固定のスペーサを作成します。
-     * @param width 横サイズ
-     * @param hight 縦サイズ
-     * @return スペーサ
-     */
-    public static JComponent createSpacer(int width, int hight) {
-        JLabel label = new JLabel();
-        ensureSize(label, width, hight);
-        return label;
-    }
+		public void setSize(int width, int height) {
+		}
 
-    /**
-     * 指定したコンポーネントに固定のサイズを設定します。
-     * @param compo コンポーネント
-     * @param width 横サイズ
-     * @param hight 縦サイズ
-     */
-    public static void ensureSize(JComponent compo, int width, int hight) {
-        Dimension dim = new Dimension(width, hight);
-        compo.setPreferredSize(dim);
-        compo.setMinimumSize(dim);
-        compo.setMaximumSize(dim);
-    }
-    
-    /**
-     * 縦スクロール可能なスクロールペインを作成します。
-     * @param compo ビューポートに表示するコンポーネント
-     * @return スクロールペイン
-     */
-    public static JScrollPane createScrollPane(JComponent compo) {
-        JScrollPane scrollPane = new JScrollPane(compo);
-        setupVerticalScrollPane(scrollPane);
-        return scrollPane;
-    }
-    
-    /**
-     * スクロールペインに縦スクロール設定します。
-     * @param scrollPane スクロールペイン
-     */
-    public static void setupVerticalScrollPane(JScrollPane scrollPane) {
-        scrollPane.setHorizontalScrollBarPolicy(
-            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollPane.setVerticalScrollBarPolicy(
-            JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-    }
-    
-    /**
-     * 指定したコンポーネント配列に設定されているフォントメトリクスから
-     * 最も長いテキストの幅に各コンポーネントの幅を合わせます。
-     * @param compos コンポーネント配列
-     */
-    public static void ensureMaxFontWidth(JComponent[] compos) {
+		public void setSize(Dimension2D d) {
+		}
+	};
 
-        int maxWidth = 0;
-        
-        for (JComponent compo : compos) {
-            FontMetrics metrics = compo.getFontMetrics(compo.getFont());
-            int width = 0;
+	/**
+	 * コンストラクタです。生成不可。
+	 */
+	private ComponentFactory() {
+	}
 
-            if (compo instanceof JLabel) {
-                width = metrics.stringWidth(((JLabel) compo).getText());
-            } else if (compo instanceof JComboBox) {
-                ComboBoxModel model = ((JComboBox) compo).getModel();
-                for (int j = 0; j < model.getSize(); j++) {
-                    width = metrics.stringWidth(model.getElementAt(j).toString());
-                    width += 30; //プルダウン部分を加算
-                    if (width > maxWidth ) maxWidth = width;
-                }
-            } else {
-                throw new IllegalArgumentException(
-                "引数は JLabel[] または JComboBox[] でなければなりません。");
-            }
+	/**
+	 * このアプリケーションがサポートするコンポーネント最大サイズを作成します。 この Dimension は通常の Dimension
+	 * と異なりサイズを変更するメソッドを 呼び出してもサイズが変わらない不変クラスです。
+	 * 
+	 * @return コンポーネントの最大サイズ
+	 */
+	public static Dimension createMaxDimension() {
+		return maxDimension;
+	}
 
-            if (width > maxWidth ) maxWidth = width;
-        }
-        
-        for (JComponent compo : compos) {
-            ComponentFactory.ensureSize(compo, maxWidth, 20);
-        }
-    }
+	/**
+	 * サイズ固定のスペーサを作成します。
+	 * 
+	 * @param width
+	 *            横サイズ
+	 * @param hight
+	 *            縦サイズ
+	 * @return スペーサ
+	 */
+	public static JComponent createSpacer(int width, int hight) {
+		JLabel label = new JLabel();
+		ensureSize(label, width, hight);
+		return label;
+	}
+
+	/**
+	 * 指定したコンポーネントに固定のサイズを設定します。
+	 * 
+	 * @param compo
+	 *            コンポーネント
+	 * @param width
+	 *            横サイズ
+	 * @param hight
+	 *            縦サイズ
+	 */
+	public static void ensureSize(JComponent compo, int width, int hight) {
+		Dimension dim = new Dimension(width, hight);
+		compo.setPreferredSize(dim);
+		compo.setMinimumSize(dim);
+		compo.setMaximumSize(dim);
+	}
+
+	/**
+	 * 縦スクロール可能なスクロールペインを作成します。
+	 * 
+	 * @param compo
+	 *            ビューポートに表示するコンポーネント
+	 * @return スクロールペイン
+	 */
+	public static JScrollPane createScrollPane(JComponent compo) {
+		JScrollPane scrollPane = new JScrollPane(compo);
+		setupVerticalScrollPane(scrollPane);
+		return scrollPane;
+	}
+
+	/**
+	 * スクロールペインに縦スクロール設定します。
+	 * 
+	 * @param scrollPane
+	 *            スクロールペイン
+	 */
+	public static void setupVerticalScrollPane(JScrollPane scrollPane) {
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+	}
+
+	/**
+	 * 指定したコンポーネント配列に設定されているフォントメトリクスから 最も長いテキストの幅に各コンポーネントの幅を合わせます。
+	 * 
+	 * @param compos
+	 *            コンポーネント配列
+	 */
+	public static void ensureMaxFontWidth(JComponent[] compos) {
+
+		int maxWidth = 0;
+
+		for (JComponent compo : compos) {
+			FontMetrics metrics = compo.getFontMetrics(compo.getFont());
+			int width = 0;
+
+			if (compo instanceof JLabel) {
+				width = metrics.stringWidth(((JLabel) compo).getText());
+			} else if (compo instanceof JComboBox) {
+				@SuppressWarnings("unchecked")
+				ComboBoxModel<Object> model = ((JComboBox<Object>) compo).getModel();
+				for (int j = 0; j < model.getSize(); j++) {
+					width = metrics.stringWidth(model.getElementAt(j).toString());
+					width += 30; // プルダウン部分を加算
+					if (width > maxWidth)
+						maxWidth = width;
+				}
+			} else {
+				throw new IllegalArgumentException("引数は JLabel[] または JComboBox[] でなければなりません。");
+			}
+
+			if (width > maxWidth)
+				maxWidth = width;
+		}
+
+		for (JComponent compo : compos) {
+			ComponentFactory.ensureSize(compo, maxWidth, 20);
+		}
+	}
 }
--- src/mergedoc/ui/FileChooserField.java
+++ src/mergedoc/ui/FileChooserField.java
@@ -38,307 +38,314 @@ import org.apache.commons.logging.LogFactory;
 
 /**
  * ファイル選択フィールドです。
+ * 
  * @author Shinji Kashihara
  */
 public class FileChooserField extends JPanel {
 
-    /** ロガー */
-    private static final Log log = LogFactory.getLog(FileChooserField.class);
+	/** ロガー */
+	private static final Log log = LogFactory.getLog(FileChooserField.class);
 
+	/** ラベル部 */
+	private final JLabel label = new JLabel();
 
-    /** ラベル部 */
-    private final JLabel label = new JLabel();
+	/** テキストフィールド部 */
+	private final JTextField textField = new JTextField();
 
-    /** テキストフィールド部 */
-    private final JTextField textField = new JTextField();
+	/** ボタン部 */
+	private final JButton button = new JButton();
 
-    /** ボタン部 */
-    private final JButton button = new JButton();
+	/** コンボボックス部 */
+	private final JComboBox<Object> combo = new JComboBox<Object>();
 
-    /** コンボボックス部 */
-    private final JComboBox combo = new JComboBox();
-    
-    /** ファイル選択ダイアログ */
-    private JFileChooser chooser = new JFileChooser();
+	/** ファイル選択ダイアログ */
+	private JFileChooser chooser = new JFileChooser();
 
-    /** ファイル選択時のリスナー */
-    private ActionListener chooseListener;
+	/** ファイル選択時のリスナー */
+	private ActionListener chooseListener;
 
+	/** エンコーディングデフォルト */
+	public static final String ENCODING_DEFAULT = System.getProperty("file.encoding");
 
-    /** エンコーディングデフォルト */
-    public static final String ENCODING_DEFAULT = System.getProperty("file.encoding");
+	/** エンコーディング自動判別 */
+	public static final String ENCODING_AUTO = "JISAutoDetect";
 
-    /** エンコーディング自動判別 */
-    public static final String ENCODING_AUTO = "JISAutoDetect";
+	/** 選択モードインターフェース */
+	private static interface SelectionMode {
+		void apply(JFileChooser chooser);
+	}
 
+	/** ディレクトリ選択モード */
+	public static final SelectionMode DIRECTORIES = new SelectionMode() {
+		public void apply(JFileChooser chooser) {
+			chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			chooser.removeChoosableFileFilter(chooser.getFileFilter());
+			chooser.setFileFilter(new FileFilter() {
+				public boolean accept(File f) {
+					return f.isDirectory();
+				}
 
-    /** 選択モードインターフェース */
-    private static interface SelectionMode {
-        void apply(JFileChooser chooser);
-    }
+				public String getDescription() {
+					return "ディレクトリのみ";
+				}
+			});
+		}
+	};
 
-    /** ディレクトリ選択モード */
-    public static final SelectionMode DIRECTORIES = new SelectionMode() {
-        public void apply(JFileChooser chooser) {
-            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            chooser.removeChoosableFileFilter(chooser.getFileFilter());
-            chooser.setFileFilter(new FileFilter() {
-                public boolean accept(File f) {
-                    return f.isDirectory();
-                }
-                public String getDescription() {
-                    return "ディレクトリのみ";
-                }
-            });
-        }
-    };
+	/** ZIP ファイル選択モード */
+	public static final SelectionMode ZIP_FILES = new SelectionMode() {
+		public void apply(JFileChooser chooser) {
+			chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			chooser.setFileFilter(new FileFilter() {
+				public boolean accept(File f) {
+					return f.isDirectory() || f.getName().endsWith(".zip") || f.getName().endsWith(".jar");
+				}
 
-    /** ZIP ファイル選択モード */
-    public static final SelectionMode ZIP_FILES = new SelectionMode() {
-        public void apply(JFileChooser chooser) {
-            chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            chooser.setFileFilter(new FileFilter() {
-                public boolean accept(File f) {
-                    return f.isDirectory() ||
-                           f.getName().endsWith(".zip") ||
-                           f.getName().endsWith(".jar");
-                }
-                public String getDescription() {
-                    return "*.zip, *.jar";
-                }
-            });
-        }
-    };
+				public String getDescription() {
+					return "*.zip, *.jar";
+				}
+			});
+		}
+	};
 
-    /** ZIP, TGZ ファイル選択モード */
-    public static final SelectionMode ZIP_TGZ_FILES = new SelectionMode() {
-        public void apply(JFileChooser chooser) {
-            chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            chooser.setFileFilter(new FileFilter() {
-                public boolean accept(File f) {
-                    return f.isDirectory() ||
-                           f.getName().endsWith(".zip") ||
-                           f.getName().endsWith(".jar") ||
-                           f.getName().endsWith(".tgz") ||
-                           f.getName().endsWith(".tar.gz");
-                }
-                public String getDescription() {
-                    return "*.zip, *.jar, *.tgz, *.tar.gz";
-                }
-            });
-        }
-    };
+	/** ZIP, TGZ ファイル選択モード */
+	public static final SelectionMode ZIP_TGZ_FILES = new SelectionMode() {
+		public void apply(JFileChooser chooser) {
+			chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			chooser.setFileFilter(new FileFilter() {
+				public boolean accept(File f) {
+					return f.isDirectory() || f.getName().endsWith(".zip") || f.getName().endsWith(".jar")
+							|| f.getName().endsWith(".tgz") || f.getName().endsWith(".tar.gz");
+				}
 
+				public String getDescription() {
+					return "*.zip, *.jar, *.tgz, *.tar.gz";
+				}
+			});
+		}
+	};
 
-    /**
-     * コンストラクタです。
-     */
-    public FileChooserField() {
+	/**
+	 * コンストラクタです。
+	 */
+	public FileChooserField() {
 
-        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 
-        // テキストフィールドの設定
-        int maxWidth = (int) ComponentFactory.createMaxDimension().getWidth();
-        textField.setMaximumSize(new Dimension(maxWidth, 20));
-        textField.setTransferHandler(new FileDropHandler());
+		// テキストフィールドの設定
+		int maxWidth = (int) ComponentFactory.createMaxDimension().getWidth();
+		textField.setMaximumSize(new Dimension(maxWidth, 20));
+		textField.setTransferHandler(new FileDropHandler());
 
-        // ボタンの設定
-        ComponentFactory.ensureSize(button, 20, 18);
-        button.setText("...");
+		// ボタンの設定
+		ComponentFactory.ensureSize(button, 20, 18);
+		button.setText("...");
 
-        // コンボモデルの作成
-        CharsetSortedModel charsetModel = new CharsetSortedModel();
-        charsetModel.add("EUC-JP");
-        charsetModel.add("EUC-JP-LINUX");
-        charsetModel.add("ISO-2022-JP");
-        charsetModel.add("MS932");
-        charsetModel.add("Shift_JIS");
-        charsetModel.add("UTF-16");
-        charsetModel.add("UTF-16BE");
-        charsetModel.add("UTF-16LE");
-        charsetModel.add("UTF-8");
-        charsetModel.add(ENCODING_DEFAULT);
-        charsetModel.fireAdded();
+		// コンボモデルの作成
+		CharsetSortedModel charsetModel = new CharsetSortedModel();
+		charsetModel.add("EUC-JP");
+		charsetModel.add("EUC-JP-LINUX");
+		charsetModel.add("ISO-2022-JP");
+		charsetModel.add("MS932");
+		charsetModel.add("Shift_JIS");
+		charsetModel.add("UTF-16");
+		charsetModel.add("UTF-16BE");
+		charsetModel.add("UTF-16LE");
+		charsetModel.add("UTF-8");
+		charsetModel.add(ENCODING_DEFAULT);
+		charsetModel.fireAdded();
 
-        // コンポーネントを配置
-        add(label);
-        add(ComponentFactory.createSpacer(10, 0));
-        add(textField);
-        add(button);
-        add(ComponentFactory.createSpacer(5, 0));
-        add(combo);
+		// コンポーネントを配置
+		add(label);
+		add(ComponentFactory.createSpacer(10, 0));
+		add(textField);
+		add(button);
+		add(ComponentFactory.createSpacer(5, 0));
+		add(combo);
 
-        // ファイル選択ダイアログの設定
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                int result = chooser.showOpenDialog(FileChooserField.this);
-                if (result == JFileChooser.APPROVE_OPTION) {
-                    setFile(chooser.getSelectedFile());
-                    if (chooseListener != null) {
-                        chooseListener.actionPerformed(e);
-                    }
-                }
-            }
-        });
-    }
+		// ファイル選択ダイアログの設定
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int result = chooser.showOpenDialog(FileChooserField.this);
+				if (result == JFileChooser.APPROVE_OPTION) {
+					setFile(chooser.getSelectedFile());
+					if (chooseListener != null) {
+						chooseListener.actionPerformed(e);
+					}
+				}
+			}
+		});
+	}
 
-    /**
-     * ファイル選択ダイアログの選択モードを設定します。
-     * @param mode 選択モード
-     */
-    public void setSelectionMode(SelectionMode mode) {
-        chooser = new JFileChooser();
-        mode.apply(chooser);
-    }
-    
-    /**
-     * ファイル選択時のリスナーをセットします．
-     * @param chooseListener ファイル選択時のリスナー
-     */    
-    public void setChooseListener(ActionListener chooseListener) {
-        this.chooseListener = chooseListener;
-    }
+	/**
+	 * ファイル選択ダイアログの選択モードを設定します。
+	 * 
+	 * @param mode
+	 *            選択モード
+	 */
+	public void setSelectionMode(SelectionMode mode) {
+		chooser = new JFileChooser();
+		mode.apply(chooser);
+	}
 
-    /**
-     * ラベルを取得します。
-     * @return ラベル
-     */
-    public JLabel getLabel() {
-        return label;
-    }
+	/**
+	 * ファイル選択時のリスナーをセットします．
+	 * 
+	 * @param chooseListener
+	 *            ファイル選択時のリスナー
+	 */
+	public void setChooseListener(ActionListener chooseListener) {
+		this.chooseListener = chooseListener;
+	}
 
-    /**
-     * ファイルを設定します。
-     * @param file ファイル
-     */
-    public void setFile(File file) {
-        textField.setText(file.getPath());
-        chooser.setSelectedFile(file);
-    }
+	/**
+	 * ラベルを取得します。
+	 * 
+	 * @return ラベル
+	 */
+	public JLabel getLabel() {
+		return label;
+	}
 
-    /**
-     * ファイルを取得します。
-     * @return ファイル
-     */
-    public File getFile() {
-        return new File(textField.getText());
-    }
-    
-    /**
-     * コンボボックスを取得します。
-     * @return コンボボックス
-     */
-    public JComboBox getComboBox() {
-        return combo;
-    }
+	/**
+	 * ファイルを設定します。
+	 * 
+	 * @param file
+	 *            ファイル
+	 */
+	public void setFile(File file) {
+		textField.setText(file.getPath());
+		chooser.setSelectedFile(file);
+	}
 
-    /**
-     * 文字セット名を昇順で保持するコンボボックスモデルです。<p>
-     * 
-     * プラットフォームでサポートされない文字セット名が追加された場合は無視します。
-     * その判定は String のコンストラクタ {@link String(byte[],String)}
-     * を使用します。{@link Charset#isSupported} は JISAutoDetect の判定が
-     * 出来ないことや、JDK1.4.1 では ISO-2022-JP の場合 false を返すため
-     * 使用されません。<p>
-     * 
-     * なお、要素を削除は今のところ考慮されておらず、削除した場合の動作は保証しません。
-     */
-    private class CharsetSortedModel extends DefaultComboBoxModel {
+	/**
+	 * ファイルを取得します。
+	 * 
+	 * @return ファイル
+	 */
+	public File getFile() {
+		return new File(textField.getText());
+	}
 
-        private final Set<Object> charsetNames;
+	/**
+	 * コンボボックスを取得します。
+	 * 
+	 * @return コンボボックス
+	 */
+	public JComboBox<Object> getComboBox() {
+		return combo;
+	}
 
-        private CharsetSortedModel() {
-            charsetNames = new TreeSet<Object>();
-        }
+	/**
+	 * 文字セット名を昇順で保持するコンボボックスモデルです。
+	 * <p>
+	 * 
+	 * プラットフォームでサポートされない文字セット名が追加された場合は無視します。 その判定は String のコンストラクタ
+	 * {@link String(byte[],String)} を使用します。{@link Charset#isSupported} は
+	 * JISAutoDetect の判定が 出来ないことや、JDK1.4.1 では ISO-2022-JP の場合 false を返すため
+	 * 使用されません。
+	 * <p>
+	 * 
+	 * なお、要素を削除は今のところ考慮されておらず、削除した場合の動作は保証しません。
+	 */
+	private class CharsetSortedModel extends DefaultComboBoxModel<Object> {
 
-        private CharsetSortedModel(Set<Object> set) {
-            super(set.toArray());
-            charsetNames = set;
-        }
-        
-        private boolean isSupported(Object charset) {
-            try {
-                new String(new byte[0], charset.toString());
-            } catch (UnsupportedEncodingException e) {
-                return false;
-            }
-            return true;
-        }
-        
-        private void add(Object charset) {
-            if (isSupported(charset)) charsetNames.add(charset);
-        }
-        
-        private void fireAdded() {
-            combo.setModel(new CharsetSortedModel(charsetNames));
-        }
-        
-        public void addElement(Object charset) {
-            if (isSupported(charset) && !charsetNames.contains(charset)) {
-                charsetNames.add(charset);
-                fireAdded();
-            }
-        }
+		private final Set<Object> charsetNames;
 
-        public void setSelectedItem(Object charset) {
-            if (charset != null) {
-                if (!charsetNames.contains(charset)) {
-                    addElement(charset);
-                }
-                super.setSelectedItem(charset);
-            }
-        }
-    }
+		private CharsetSortedModel() {
+			charsetNames = new TreeSet<Object>();
+		}
 
-    /**
-     * 動作プラットフォームからのテキストフィールドへのファイルのドロップを受け付ける
-     * ための転送ハンドラです。ファイルのドロップ以外の転送、例えば文字列のドロップや
-     * カット&ペーストなどはテキストフィールドが持つオリジナルの転送ハンドラに委譲します。
-     */
-    private class FileDropHandler extends TransferHandler {
+		private CharsetSortedModel(Set<Object> set) {
+			super(set.toArray());
+			charsetNames = set;
+		}
 
-        TransferHandler originHandler = textField.getTransferHandler();
+		private boolean isSupported(Object charset) {
+			try {
+				new String(new byte[0], charset.toString());
+			} catch (UnsupportedEncodingException e) {
+				return false;
+			}
+			return true;
+		}
 
-        public void exportToClipboard(JComponent comp, Clipboard clipboard, int action) {
-            originHandler.exportToClipboard(comp, clipboard, action);
-        }
+		private void add(Object charset) {
+			if (isSupported(charset))
+				charsetNames.add(charset);
+		}
 
-        public int getSourceActions(JComponent comp) {
-            return originHandler.getSourceActions(comp);
-        }
+		private void fireAdded() {
+			combo.setModel(new CharsetSortedModel(charsetNames));
+		}
 
-        public boolean canImport(JComponent comp, DataFlavor[] flavors) {
-            for (DataFlavor flavor : flavors) {
-                if (flavor.isFlavorJavaFileListType()) {
-                    return true;
-                }
-            }
-            return originHandler.canImport(comp, flavors);
-        }
+		public void addElement(Object charset) {
+			if (isSupported(charset) && !charsetNames.contains(charset)) {
+				charsetNames.add(charset);
+				fireAdded();
+			}
+		}
 
-        public boolean importData(JComponent comp, Transferable t) {
-            boolean imported = false;
-            if (t.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
-                try {
-                    List files = (List) t.getTransferData(DataFlavor.javaFileListFlavor);
-                    File file = (File) files.get(0);
-                    setFile(file);
-                    textField.repaint();
-                    imported = true;
-                } catch (UnsupportedFlavorException e) {
-                } catch (IOException e) {
-                }
-            } else {
-                imported = originHandler.importData(comp, t);
-            }
-            if (imported && chooseListener != null && getFile().exists()) {
-                SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        chooseListener.actionPerformed(null);
-                    }
-                });
-            }
-            return imported;
-        }
-    }
+		public void setSelectedItem(Object charset) {
+			if (charset != null) {
+				if (!charsetNames.contains(charset)) {
+					addElement(charset);
+				}
+				super.setSelectedItem(charset);
+			}
+		}
+	}
+
+	/**
+	 * 動作プラットフォームからのテキストフィールドへのファイルのドロップを受け付ける
+	 * ための転送ハンドラです。ファイルのドロップ以外の転送、例えば文字列のドロップや
+	 * カット&ペーストなどはテキストフィールドが持つオリジナルの転送ハンドラに委譲します。
+	 */
+	private class FileDropHandler extends TransferHandler {
+
+		TransferHandler originHandler = textField.getTransferHandler();
+
+		public void exportToClipboard(JComponent comp, Clipboard clipboard, int action) {
+			originHandler.exportToClipboard(comp, clipboard, action);
+		}
+
+		public int getSourceActions(JComponent comp) {
+			return originHandler.getSourceActions(comp);
+		}
+
+		public boolean canImport(JComponent comp, DataFlavor[] flavors) {
+			for (DataFlavor flavor : flavors) {
+				if (flavor.isFlavorJavaFileListType()) {
+					return true;
+				}
+			}
+			return originHandler.canImport(comp, flavors);
+		}
+
+		public boolean importData(JComponent comp, Transferable t) {
+			boolean imported = false;
+			if (t.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+				try {
+					@SuppressWarnings("unchecked")
+					List<Object> files = (List<Object>) t.getTransferData(DataFlavor.javaFileListFlavor);
+					File file = (File) files.get(0);
+					setFile(file);
+					textField.repaint();
+					imported = true;
+				} catch (UnsupportedFlavorException e) {
+				} catch (IOException e) {
+				}
+			} else {
+				imported = originHandler.importData(comp, t);
+			}
+			if (imported && chooseListener != null && getFile().exists()) {
+				SwingUtilities.invokeLater(new Runnable() {
+					public void run() {
+						chooseListener.actionPerformed(null);
+					}
+				});
+			}
+			return imported;
+		}
+	}
 }
